26ddd2203ce98626a94199ac2fd0b37c3550eb59
fgei <gei.fff@gmail.com>
2017-01-30 14:49:47 +0100

    some more work on assistant, investigate lifecycle and ViewNode class

--- app/src/main/java/com/duckduckgo/mobile/android/assistant/AssistantSession.java
+++ app/src/main/java/com/duckduckgo/mobile/android/assistant/AssistantSession.java
@@ -10,14 +10,20 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.service.voice.VoiceInteractionSession;
 import android.util.Log;
+import android.view.View;
 
 import com.duckduckgo.mobile.android.activity.DuckDuckGo;
 
 /**
  * Created by fgei on 1/26/17.
  */
-@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+@TargetApi(Build.VERSION_CODES.M)
 public class AssistantSession extends VoiceInteractionSession {
+
+    public static final String EXTRA_SEARCH = "extra_search";
+
+    private String search;
+
     public AssistantSession(Context context) {
         super(context);
     }
@@ -26,6 +32,22 @@ public class AssistantSession extends VoiceInteractionSession {
     public void onHandleAssist(Bundle data, AssistStructure structure, AssistContent content) {
         super.onHandleAssist(data, structure, content);
         Log.e("assist_test", "on handle assist");
+        CharSequence text = null;
+        for(int i=0; i<structure.getWindowNodeCount(); i++) {
+            AssistStructure.WindowNode node = structure.getWindowNodeAt(i);
+            AssistStructure.ViewNode viewNode = node.getRootViewNode();
+            printViewNode(viewNode);
+        }
+        Intent intent = new Intent(getContext(), DuckDuckGo.class);
+        intent.setAction(Intent.ACTION_ASSIST);
+        getContext().startActivity(intent);
+    }
 
+    private void printViewNode(AssistStructure.ViewNode viewNode) {
+        Log.e("assist_test", "text: "+viewNode.getText()+" class name"+viewNode.getClassName()+" extras!=null: "+(viewNode.getExtras()!=null));
+        for(int i=0; i<viewNode.getChildCount(); i++) {
+            AssistStructure.ViewNode child = viewNode.getChildAt(i);
+            printViewNode(child);
+        }
     }
 }
